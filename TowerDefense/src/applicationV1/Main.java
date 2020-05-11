d4a5097459463a3a474cf88dc581a520eb4f08a5
menmendemil <menmendemil@gmail.com>
2020-05-11 11:29:44 +0200

    mise à jour main

--- TowerDefense/src/applicationV1/Main.java
+++ TowerDefense/src/applicationV1/Main.java
@@ -1,5 +1,27 @@
 package applicationV1;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
 
+public class Main extends Application{
+
+	public void start(Stage primaryStage) {
+		
+		try {
+			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getClassLoader().getResource("applicationV1/vue/GameView.fxml"));
+			Scene scene = new Scene(root,900,900);
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
+
