2101d0aa734209ba27fdc9d2a7366763b234d04c
menmendemil <menmendemil@gmail.com>
2020-05-11 17:41:45 +0200

    changements TileMap

--- TowerDefense/src/byteDefense/model/tiles/TileMap.java
+++ TowerDefense/src/byteDefense/model/tiles/TileMap.java
@@ -6,13 +6,15 @@ import java.io.FileReader;
 public class TileMap {
 	
 	public static final int TILES_SIZE = 13;
-	private static final int CORNER = 1;
-	private static final int HORIZONTAL_PATH = 2;
-	private static final int VERTICAL_PATH = 3;
-	private static final int VERTICAL_WALL = 4;
-	private static final int INTERSECTION_PATH = 5;
-	private static final int HORIZONTAL_WALL = 6;
-	private static final int TOWER_ZONE = 7;
+	
+	// Données correspondant aux composantes visuelles (tiles) de la tilemap 
+	public static final int CORNER = 1;
+	public static final int HORIZONTAL_PATH = 2;
+	public static final int VERTICAL_PATH = 3;
+	public static final int VERTICAL_WALL = 4;
+	public static final int INTERSECTION_PATH = 5;
+	public static final int HORIZONTAL_WALL = 6;
+	public static final int TOWER_ZONE = 7;
 
 	private int[] tiles;
 	
@@ -26,12 +28,14 @@ public class TileMap {
 			FileReader input = new FileReader("./resources/tiles.txt");
 			BufferedReader br = new BufferedReader(input);
 		    String line = null;
+		    
 		    while ((line = br.readLine()) != null) {
 		    	String[] values = line.split(", ");
 		    	for (int i = 0; i < values.length; i++) {
 		    		this.tiles[i] = Integer.parseInt(values[i]);
 		    	}
 		    }
+		    
 		    br.close();
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -41,34 +45,4 @@ public class TileMap {
 	public int getCase(int x, int y) {
 		return tiles[x + TILES_SIZE * y];
 	}
-	
-	public String getCaseNamePng(int mapCase) {
-		String nameUrlTile = "";
-		
-		switch (mapCase) {
-			case TileMap.CORNER:
-				nameUrlTile = "corner";
-				break;
-			case TileMap.HORIZONTAL_PATH:
-				nameUrlTile = "horizontal_wall";
-				break;
-			case TileMap.VERTICAL_PATH:
-				nameUrlTile = "vertical_wall";
-				break;
-			case TileMap.VERTICAL_WALL:
-				nameUrlTile = "vertical_path";
-				break;
-			case TileMap.INTERSECTION_PATH:
-				nameUrlTile = "intersection_path";
-				break;
-			case TileMap.HORIZONTAL_WALL:
-				nameUrlTile = "horizontal_path";
-				break;
-			case TileMap.TOWER_ZONE:
-				nameUrlTile = "tower_zone";
-				break;
-		}
-		return nameUrlTile;
-	}
-	
 }
