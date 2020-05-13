dd821043f3dc721b404bf45bf1e8e4ef1aa95ae8
zou-simon <zou-simon@hotmail.fr>
2020-05-13 11:58:26 +0200

    mise à jour du Controller

--- TowerDefense/src/byteDefense/controller/Controller.java
+++ TowerDefense/src/byteDefense/controller/Controller.java
@@ -5,9 +5,12 @@ import java.util.ResourceBundle;
 
 import byteDefense.model.tiles.TileMap;
 import byteDefense.view.TileMapView;
-import javafx.fxml.Initializable;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.TilePane;
+import javafx.util.Duration;
 
 public class Controller implements Initializable {
 	
@@ -16,11 +19,37 @@ public class Controller implements Initializable {
     @FXML
     private TilePane gameBoard;
     
+    private Timeline gameLoop;
+    
+    private int time;
+    
     @Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		this.map = new TileMap();
-		new TileMapView(map, gameBoard);
+		new TileMapView(this.map, this.gameBoard);
+		
+		initAnimation();
+		this.gameLoop.play();
 	}
 	
+    private void initAnimation() {
+		this.gameLoop = new Timeline();
+		this.time = 0;
+		this.gameLoop.setCycleCount(Timeline.INDEFINITE);
+
+		KeyFrame kf = new KeyFrame(
+				Duration.seconds(0.1), 
+				(ev ->{
+					if (this.time == 100) {
+						System.out.println("fini");
+						gameLoop.stop();
+					} else if (this.time % 5 == 0) {
+						System.out.println("un tour");
+					}
+					time++;
+				})
+				);
+		this.gameLoop.getKeyFrames().add(kf);
+	}
     
 }
