f5989620bb9622dedb690f640b571ee0adf429b3
menmendemil <menmendemil@gmail.com>
2020-05-13 11:10:16 +0200

    changement modèle ennemis

--- TowerDefense/src/byteDefense/model/ennemies/Ennemy.java
+++ TowerDefense/src/byteDefense/model/ennemies/Ennemy.java
@@ -1,18 +1,21 @@
 package byteDefense.model.ennemies;
 
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
 public abstract class Ennemy {
 
 	private int id;
-	private int x;
-	private int y;
-	private StatisticsEnnemy stats;
+	private IntegerProperty xProperty;
+	private IntegerProperty yProperty;
+	private int hp;
 	
 	private static int counterId = 0;
 	
-	public Ennemy(int x, int y, StatisticsEnnemy stats) {
-		this.x = x;
-		this.y = y;
-		this.stats = stats;
+	public Ennemy(int x, int y) {
+		this.xProperty = new SimpleIntegerProperty(x);
+		this.yProperty = new SimpleIntegerProperty(y);
+		this.hp = 0;
 		this.id = counterId;
 		counterId++;
 	}
@@ -21,52 +24,49 @@ public abstract class Ennemy {
 		return this.id;
 	}
 
+	public IntegerProperty getXProperty() {
+		return this.xProperty;
+	}
+	
 	public int getX() {
-		return this.x;
+		return this.xProperty.getValue();
 	}
 	
 	public void setX(int newX) {
-		this.x = newX;
+		this.xProperty.setValue(newX);
+	}
+	
+	public IntegerProperty getYProperty() {
+		return this.yProperty;
 	}
 	
 	public int getY() {
-		return this.y;
+		return this.yProperty.getValue();
 	}
 	
 	public void setY(int newY) {
-		this.y = newY;
+		this.yProperty.setValue(newY);
 	}	
 	
 	public int getHp() {
-		return this.stats.getHp();
+		return this.hp;
 	}
 	
 	public void decrementHp(int lostHp) {
-		this.stats.setHp(this.stats.getHp() - lostHp);
+		this.hp -= lostHp;
 	}
 
 	public boolean isAlive() {
-		return this.stats.getHp() > 0;
+		return this.hp > 0;
 	}
 	
-	public int getAttack() {
-		return this.stats.getAttack();
-	}
+	public abstract int getAttack();
 
-	public int getDefense() {
-		return this.stats.getDefense();
-	}
+	public abstract int getDefense();
 
-	public int getAttackSpeed() {
-		return this.stats.getAttackSpeed();
-	}
+	public abstract int getAttackSpeed();
 
-	public int getAttackRange() {
-		return this.stats.getAttackRange();
-	}
-
-	public int getCost() {
-		return this.stats.getLoot();
-	}
-
+	public abstract int getAttackRange();
+	
+	public abstract int getLoot();
 }
--- TowerDefense/src/byteDefense/model/ennemies/Ransomware.java
+++ TowerDefense/src/byteDefense/model/ennemies/Ransomware.java
@@ -2,8 +2,33 @@ package byteDefense.model.ennemies;
 
 public class Ransomware extends Ennemy {
 
+	private static final int ATTACK = 0;
+	private static final int DEFENSE = 0;
+	private static final int ATTACK_SPEED = 0;
+	private static final int ATTACK_RANGE = 0;
+	private static final int LOOT = 0;
+	
 	public Ransomware() {
-		super(0, 0, new StatisticsEnnemy(0, 0, 0, 0, 0, 0));
+		super(0, 0);
 	}
 	
+	public int getAttack() {
+		return ATTACK;
+	}
+
+	public int getDefense() {
+		return DEFENSE;
+	}
+
+	public int getAttackSpeed() {
+		return ATTACK_SPEED;
+	}
+
+	public int getAttackRange() {
+		return ATTACK_RANGE;
+	}
+
+	public int getLoot() {
+		return LOOT;
+	}
 }
