c8a74ca3cd9116f44bf0f039edecd1e8c048d734
KO-03 <58856041+KO-03@users.noreply.github.com>
2020-05-13 13:18:22 +0200

    maj fichiers

--- TowerDefense/src/byteDefense/controller/Controller.java
+++ TowerDefense/src/byteDefense/controller/Controller.java
@@ -1,3 +1,10 @@
+/*
+ * Controller.java
+ * Cette classe fait le lien entre le modèle et la vue, ses responsabilités sont :
+ * - initialiser la TileMap
+ * - gérer la gameLoop
+ */
+
 package byteDefense.controller;
 
 import java.net.URL;
@@ -5,22 +12,48 @@ import java.util.ResourceBundle;
 
 import byteDefense.model.TileMap;
 import byteDefense.view.TileMapView;
-import javafx.fxml.Initializable;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.layout.TilePane;
+import javafx.util.Duration;
 
 public class Controller implements Initializable {
 	
-	private TileMap map;
-	
-    @FXML
+	@FXML
     private TilePane gameBoard;
+	
+	private TileMap map;
+    private Timeline gameLoop;
+    private int time;
     
     @Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		this.map = new TileMap();
-		TileMapView tmv = new TileMapView(map, gameBoard);
+		new TileMapView(this.map, this.gameBoard);
+		
+		initAnimation();
+		this.gameLoop.play();
 	}
 	
-    
+    private void initAnimation() {
+		this.gameLoop = new Timeline();
+		this.time = 0;
+		this.gameLoop.setCycleCount(Timeline.INDEFINITE);
+
+		KeyFrame kf = new KeyFrame(
+				Duration.seconds(0.1), 
+				(ev ->{
+					if (this.time == 100) {
+						System.out.println("fini");
+						gameLoop.stop();
+					} else if (this.time % 5 == 0) {
+						System.out.println("un tour");
+					}
+					time++;
+				})
+				);
+		this.gameLoop.getKeyFrames().add(kf);
+	}
 }
--- TowerDefense/src/byteDefense/model/ennemies/Ransomware.java
+++ TowerDefense/src/byteDefense/model/ennemies/Ransomware.java
@@ -1,9 +1,39 @@
+/*
+ * Ransomware.java
+ * Cette classe représente un objet Ransomware.
+ */
+
 package byteDefense.model.ennemies;
 
 public class Ransomware extends Ennemy {
 
+	private static final int ATTACK = 0;
+	private static final int DEFENSE = 0;
+	private static final int ATTACK_SPEED = 0;
+	private static final int ATTACK_RANGE = 0;
+	private static final int LOOT = 0;
+	
 	public Ransomware() {
-		super(0, 0, 0, 0, 0, 0, 0, 0, "character");
+		super(0, 0);
 	}
 	
+	public int getAttack() {
+		return ATTACK;
+	}
+
+	public int getDefense() {
+		return DEFENSE;
+	}
+
+	public int getAttackSpeed() {
+		return ATTACK_SPEED;
+	}
+
+	public int getAttackRange() {
+		return ATTACK_RANGE;
+	}
+
+	public int getLoot() {
+		return LOOT;
+	}
 }
